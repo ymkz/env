6b665e49c4a8c0171d520d6db9393f08268b0849
ymkz <ymkzly@gmail.com>
2018-12-09 00:39:56 +0900

    chore: add root editorconfig

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -nfs $HOME/workspace/ghq/github.com/naoya3e/dotfiles/gitconfig $HOME/.gitconf
 ln -nfs $HOME/workspace/ghq/github.com/naoya3e/dotfiles/globalignore $HOME/.globalignore
 ln -nfs $HOME/workspace/ghq/github.com/naoya3e/dotfiles/zshenv $HOME/.zshenv
 ln -nfs $HOME/workspace/ghq/github.com/naoya3e/dotfiles/zshrc $HOME/.zshrc
+ln -nfs $HOME/workspace/ghq/github.com/naoya3e/dotfiles/editorconfig $HOME/.editorconfig

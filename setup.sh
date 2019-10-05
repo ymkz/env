53937e77afa260d4ea59f8931a9982ed16c568e1
ymkz <ymkzly@gmail.com>
2019-10-05 15:18:22 +0900

    feat(prompt): add starship prompt configuration

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/zshrc $HOME/.zshrc
 ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/editorconfig $HOME/.editorconfig
 ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/vimrc $HOME/.vimrc
 ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/latexmk $HOME/.latexmk
+ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/starship $HOME/.config/starship.toml

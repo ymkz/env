cde6c4a01f7731c808d02b40f18e659d1bc06d78
ymkz <ymkzly@gmail.com>
2020-01-15 21:40:06 +0900

    chore: update install scripts

--- linux/scripts/bootstrap.sh
+++ linux/scripts/bootstrap.sh
@@ -7,7 +7,7 @@ if [ -e "$HOME/dotfiles-master" ] || [ -e "$HOME/workspace/ghq/github.com/ymkz/d
   exit 1
 fi
 
-wget https://github.com/ymkz/dotfiles/archive/master.zip -o $HOME/dotfiles.zip
+wget https://github.com/ymkz/dotfiles/archive/master.zip -O $HOME/dotfiles.zip
 unzip $HOME/dotfiles.zip
 rm $HOME/dotfiles.zip
 cd $HOME/dotfiles-master/linux
@@ -32,7 +32,6 @@ sudo apt install -y \
   libffi-dev \
   libtool \
   unixodbc-dev \
-  unzip \
   curl \
   git \
   exa \
@@ -40,14 +39,48 @@ sudo apt install -y \
 
 chsh -s $(which zsh)
 
+echo "Install GHQ binary from GitHub Release"
+wget https://github.com/motemen/ghq/releases/download/v1.0.1/ghq_linux_amd64.zip -O $HOME/ghq.zip
+unzip $HOME/ghq.zip
+rm $HOME/ghq.zip
+mkdir -p $HOME/workspace/bin
+cp $HOME/ghq_linux_amd64/ghq $HOME/workspace/bin
+rm -r $HOME/ghq_linux_amd64
+
 cp ./gitconfig $HOME/.gitconfig
-ghq get git@github.com:ymkz/dotfiles.git
+$HOME/workspace/bin/ghq get ymkz/dotfiles
 rm $HOME/.gitconfig
 
 scripts/symlink.sh
 echo
 
-scripts/install.sh
-echo
+echo "Install zplug"
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+echo "Install starship shell prompt"
+curl -fsSL https://starship.rs/install.sh | bash
+
+echo "Install asdf-vm from Git"
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.6
+source $HOME/.zshrc
+asdf plugin-add nodejs
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+asdf plugin-add yarn
+asdf install nodejs 12.14.1
+asdf install yarn 1.21.1
+asdf global nodejs 12.14.1
+asdf global yarn 1.21.1
+
+# reference: UbuntuにVSCodeをインストールする3つの方法 - https://qiita.com/yoshiyasu1111/items/e21a77ed68b52cb5f7c8
+echo "Install VSCode"
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > $HOME/microsoft.gpg
+sudo install -o root -g root -m 644 $HOME/microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install code -y
+
+echo "Install Cascadia Code font from GitHub Release"
+mkdir $HOME/.fonts
+curl https://github.com/microsoft/cascadia-code/releases/download/v1911.21/CascadiaPL.ttf -o $HOME/.fonts/CascadiaPL.ttf
 
 rm -rf $HOME/dotfiles-master
--- linux/scripts/symlink.sh
+++ linux/scripts/symlink.sh
@@ -2,13 +2,13 @@
 
 set -eu
 
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/asdfrc $HOME/.asdfrc
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/editorconfig $HOME/.editorconfig
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/gitconfig $HOME/.gitconfig
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/globalgitignore $HOME/.globalgitignore
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/latexmkrc $HOME/.latexmkrc
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/npmrc $HOME/.npmrc
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/starship.toml $HOME/.config/starship.toml
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/vimrc $HOME/.vimrc
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/zshenv $HOME/.zshenv
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/zshrc $HOME/.zshrc
+ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/asdfrc $HOME/.asdfrc
+ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/editorconfig $HOME/.editorconfig
+ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/gitconfig $HOME/.gitconfig
+ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/globalgitignore $HOME/.globalgitignore
+ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/latexmkrc $HOME/.latexmkrc
+ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/npmrc $HOME/.npmrc
+ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/starship.toml $HOME/.config/starship.toml
+ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/vimrc $HOME/.vimrc
+ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/zshenv $HOME/.zshenv
+ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/zshrc $HOME/.zshrc

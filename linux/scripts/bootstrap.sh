e084012ba063ec2ca258f2a4b579e3ea588047af
ymkz <ymkzly@gmail.com>
2020-01-28 07:10:48 +0900

    chore: add vscode apt update with yes

--- linux/scripts/bootstrap.sh
+++ linux/scripts/bootstrap.sh
@@ -95,7 +95,7 @@ echo "Install VSCode"
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > $HOME/microsoft.gpg
 sudo install -o root -g root -m 644 $HOME/microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt update
+sudo apt update -y
 sudo apt install code -y
 
 echo "Install Cascadia Code font from GitHub Release"

efba6e9b60a50267f7f16c2c3df21a6d11a7822f
ymkz <ymkzly@gmail.com>
2020-02-07 16:50:04 +0900

    feat: add docker-compose install script

--- linux/scripts/bootstrap.sh
+++ linux/scripts/bootstrap.sh
@@ -89,6 +89,10 @@ sudo apt update -y
 sudo apt install docker-ce -y
 sudo usermod -aG docker $(whoami)
 
+echo ">>> Install Docker Compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 echo ">>> Change default shell"
 sudo chsh $USER -s $(which zsh)
 

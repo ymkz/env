4728bb11017c043635c295a5dc3380f89b71262e
ymkz <ymkzly@gmail.com>
2020-04-05 17:54:53 +0900

    chore: move zplug installation with curl to fix path

--- macos/scripts/bootstrap.sh
+++ macos/scripts/bootstrap.sh
@@ -21,6 +21,9 @@ curl -OL https://raw.githubusercontent.com/ymkz/dotfiles/master/macos/Brewfile
 brew bundle
 rm Brewfile
 
+echo ">>> Install zplug to .zplug"
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
 echo ">>> Fetch dotfiles"
 mkdir -p $HOME/workspace/ghq/github.com/ymkz
 git clone https://github.com/ymkz/dotfiles.git $HOME/workspace/ghq/github.com/ymkz/dotfiles

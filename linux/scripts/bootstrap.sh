f60656efbf5f25df5e2777f6b9e9b027cd5768e6
ymkz <ymkzly@gmail.com>
2020-05-27 22:53:36 +0900

    fix: shellcheck modified from gha error

--- linux/scripts/bootstrap.sh
+++ linux/scripts/bootstrap.sh
@@ -48,7 +48,7 @@ echo ">>> Install linuxbrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 echo ">>> Activate linuxbrew for temporarily"
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 echo ">>> Install system from linuxbrew"
 brew install bat
@@ -66,28 +66,28 @@ brew install wget
 brew install zplug
 
 echo ">>> Fetch dotfiles"
-mkdir -p $HOME/workspace/ghq/github.com/ymkz
-git clone https://github.com/ymkz/dotfiles.git $HOME/workspace/ghq/github.com/ymkz/dotfiles
+mkdir -p "$HOME"/workspace/ghq/github.com/ymkz
+git clone https://github.com/ymkz/dotfiles.git "$HOME"/workspace/ghq/github.com/ymkz/dotfiles
 
 echo ">>> Link dotfiles"
-mkdir -p $HOME/.config/bat
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/editorconfig $HOME/.editorconfig
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/gitconfig $HOME/.gitconfig
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/globalgitignore $HOME/.globalgitignore
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/starship.toml $HOME/.config/starship.toml
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/vimrc $HOME/.vimrc
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/zshenv $HOME/.zshenv
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/linux/zshrc $HOME/.zshrc
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/macos/batconfig $HOME/.config/bat/config
+mkdir -p "$HOME"/.config/bat
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/linux/editorconfig "$HOME"/.editorconfig
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/linux/gitconfig "$HOME"/.gitconfig
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/linux/globalgitignore "$HOME"/.globalgitignore
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/linux/starship.toml "$HOME"/.config/starship.toml
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/linux/vimrc "$HOME"/.vimrc
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/linux/zshenv "$HOME"/.zshenv
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/linux/zshrc "$HOME"/.zshrc
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/macos/batconfig "$HOME"/.config/bat/config
 
 # reference: UbuntuにVSCodeをインストールする3つの方法 - https://qiita.com/yoshiyasu1111/items/e21a77ed68b52cb5f7c8
 echo ">>> Install VSCode"
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > $HOME/microsoft.gpg
-sudo install -o root -g root -m 644 $HOME/microsoft.gpg /etc/apt/trusted.gpg.d/
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > "$HOME"/microsoft.gpg
+sudo install -o root -g root -m 644 "$HOME"/microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update -y
 sudo apt install code -y
-rm $HOME/microsoft.gpg
+rm "$HOME"/microsoft.gpg
 
 echo ">>> Instal VSCode Extensions"
 code --install-extension bierner.markdown-preview-github-styles
@@ -113,14 +113,14 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update -y
 sudo apt install docker-ce -y
-sudo usermod -aG docker $USER
+sudo usermod -aG docker "$USER"
 
 echo ">>> Install Docker Compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo ">>> Change default shell"
-sudo chsh $USER -s $(which zsh)
+sudo chsh "$USER" -s $(which zsh)
 
 echo ">>> ---"
 echo ">>> Done!"
--- macos/scripts/bootstrap.sh
+++ macos/scripts/bootstrap.sh
@@ -22,22 +22,22 @@ brew bundle
 rm Brewfile
 
 echo ">>> Fetch dotfiles"
-mkdir -p $HOME/workspace/ghq/github.com/ymkz
-git clone https://github.com/ymkz/dotfiles.git $HOME/workspace/ghq/github.com/ymkz/dotfiles
+mkdir -p "$HOME"/workspace/ghq/github.com/ymkz
+git clone https://github.com/ymkz/dotfiles.git "$HOME"/workspace/ghq/github.com/ymkz/dotfiles
 
 echo ">>> Link dotfiles"
-mkdir -p $HOME/.config/bat
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/macos/editorconfig $HOME/.editorconfig
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/macos/gitconfig $HOME/.gitconfig
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/macos/globalgitignore $HOME/.globalgitignore
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/macos/starship.toml $HOME/.config/starship.toml
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/macos/vimrc $HOME/.vimrc
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/macos/zshenv $HOME/.zshenv
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/macos/zshrc $HOME/.zshrc
-ln -nfs $HOME/workspace/ghq/github.com/ymkz/dotfiles/macos/batconfig $HOME/.config/bat/config
+mkdir -p "$HOME"/.config/bat
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/macos/editorconfig "$HOME"/.editorconfig
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/macos/gitconfig "$HOME"/.gitconfig
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/macos/globalgitignore "$HOME"/.globalgitignore
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/macos/starship.toml "$HOME"/.config/starship.toml
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/macos/vimrc "$HOME"/.vimrc
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/macos/zshenv "$HOME"/.zshenv
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/macos/zshrc "$HOME"/.zshrc
+ln -nfs "$HOME"/workspace/ghq/github.com/ymkz/dotfiles/macos/batconfig "$HOME"/.config/bat/config
 
 echo ">>> Configure macOS default settings"
-$HOME/workspace/ghq/github.com/ymkz/dotfiles/macos/scripts/configure.sh
+"$HOME"/workspace/ghq/github.com/ymkz/dotfiles/macos/scripts/configure.sh
 
 echo ">>> Instal VSCode Extensions"
 code --install-extension bierner.markdown-preview-github-styles
@@ -58,7 +58,7 @@ code --install-extension wmaurer.change-case
 code --install-extension yahya-gilany.vscode-clock
 
 echo ">>> Generate workspace"
-mkdir -p $HOME/workspace/cic
+mkdir -p "$HOME"/workspace/cic
 
 echo ">>> ---"
 echo ">>> Done!"

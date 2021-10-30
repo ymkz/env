8277a8e95377078ad864703f707cf42116fe3faa
ymkz <ymkz@users.noreply.github.com>
2021-10-30 13:55:10 +0900

    chore: modify vscode settings

---
+++ vscode/extensions.wsl.client.sh
@@ -0,0 +1,10 @@
+code --install-extension bierner.markdown-preview-github-styles
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension denoland.vscode-deno
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension MS-CEINTL.vscode-language-pack-ja
+code --install-extension qezhu.gitlink
+code --install-extension tamasfe.even-better-toml
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension wmaurer.change-case

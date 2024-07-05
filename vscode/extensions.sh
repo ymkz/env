d62a80be844de4f398ec229076771a48137dcdd0
ymkz <ymkz@users.noreply.github.com>
2024-07-05 22:07:32 +0900

    chore: add vscode extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,7 +1,7 @@
-code --install-extension eamodio.gitlens
 code --install-extension esbenp.prettier-vscode
-code --install-extension MS-CEINTL.vscode-language-pack-ja
 code --install-extension redhat.vscode-yaml
 code --install-extension tamasfe.even-better-toml
-code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension wmaurer.change-case
+code --install-extension unifiedjs.vscode-remark
+code --install-extension bufbuild.vscode-buf
+code --install-extension biomejs.biome

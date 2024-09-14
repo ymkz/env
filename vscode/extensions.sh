de01ff0e0d5a24030245cb76f7533f5dc367a00e
ymkz <ymkz@users.noreply.github.com>
2024-09-15 01:52:36 +0900

    chore: update vscode settings

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -2,6 +2,4 @@ code --install-extension esbenp.prettier-vscode
 code --install-extension redhat.vscode-yaml
 code --install-extension tamasfe.even-better-toml
 code --install-extension wmaurer.change-case
-code --install-extension unifiedjs.vscode-remark
-code --install-extension bufbuild.vscode-buf
 code --install-extension biomejs.biome

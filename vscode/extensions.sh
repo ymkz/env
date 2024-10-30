8d23dfb141b46f336023a19e189eea57cabcde2a
ymkz <ymkz@users.noreply.github.com>
2024-10-30 22:32:53 +0900

    chore: change change case vscode extension

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,5 +1,5 @@
 code --install-extension esbenp.prettier-vscode
 code --install-extension redhat.vscode-yaml
 code --install-extension tamasfe.even-better-toml
-code --install-extension wmaurer.change-case
+code --install-extension otnansirk.casing-convention
 code --install-extension biomejs.biome
